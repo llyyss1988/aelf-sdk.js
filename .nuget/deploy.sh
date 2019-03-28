be0680e19a2b2a8b77633affc89151cca7cdc2c7
llyyss1988 <llyyss88@163.com>
2019-03-28 14:54:31 +0800

    Update deploy.sh

--- .nuget/deploy.sh
+++ .nuget/deploy.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 HERE=$(cd "$(dirname "$0")";pwd)
+echo '${HERE}---------'
+echo ${HERE}
 cd ${HERE}
-
+echo "ls"
+ls 
 TAG=$1
 NUGET_API_KEY=$2
 
+echo "$TAG ,,,,,,"
+
 # TODO: verify version
 VERSION=`echo $TAG | cut -b 2-`
-
+echo "$VERSION"
+echo "ls"
+ls 
 NUPKG="${HERE}/AElf.JSDK.${VERSION}.nupkg"
-
+echo "$NUPKG"
 # build
 mkdir AElf.JSDK/content
 cp -r ../dist/* AElf.JSDK/content/
 dotnet build AElf.JSDK/AElf.JSDK.csproj --configuration Release -P:Version=${VERSION} -o ../
 
+
+echo "ls"
+ls 
 if [ ! -f "${NUPKG}" ]; then
     echo "package not exist: ${NUPKG}"
     exit 1
 fi
 
 # publish
-dotnet nuget push ${NUPKG} -k ${NUGET_API_KEY} -s https://api.nuget.org/v3/index.json
+#dotnet nuget push ${NUPKG} -k ${NUGET_API_KEY} -s https://api.nuget.org/v3/index.json
